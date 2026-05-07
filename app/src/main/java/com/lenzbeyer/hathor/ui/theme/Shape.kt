30d9420f1d43e7b3d6573efc19400109ed3990e8
lorenz.beyer@orca-software.com <lorenz.beyer@orca-software.com>
2026-05-07 12:22:22 +0200

    bugfix

--- app/src/main/java/com/lenzbeyer/hathor/ui/theme/Shape.kt
+++ app/src/main/java/com/lenzbeyer/hathor/ui/theme/Shape.kt
@@ -1,12 +1,15 @@
 package com.lenzbeyer.hathor.ui.theme
 
-import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+private val Sharp = RoundedCornerShape(0.dp)
 
 val AngularShapes = Shapes(
-    extraSmall = RectangleShape,
-    small      = RectangleShape,
-    medium     = RectangleShape,
-    large      = RectangleShape,
-    extraLarge = RectangleShape,
+    extraSmall = Sharp,
+    small      = Sharp,
+    medium     = Sharp,
+    large      = Sharp,
+    extraLarge = Sharp,
 )
